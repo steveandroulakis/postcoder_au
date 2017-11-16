096ee5cca48f8da110de95dfbcb90254acd0598f
Steve Androulakis <steve.androulakis@gmail.com>
2017-11-17 08:37:09 +1100

    removed redundant comments

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 const Alexa = require('alexa-sdk');
 var request = require('request');
 
 const API_PREFIX = 'http://v0.postcodeapi.com.au/suburbs/';
 const API_SUFFIX = '.json'; // admin: wordlist
 
-//=========================================================================================================================================
-//TODO: The items below this comment need your attention.
-//=========================================================================================================================================
 
-//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
-//Make sure to enclose your value in quotes, like this: const APP_ID = 'amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1';
 const APP_ID = undefined;
 
 const SKILL_NAME = 'Postcoder Australia';
